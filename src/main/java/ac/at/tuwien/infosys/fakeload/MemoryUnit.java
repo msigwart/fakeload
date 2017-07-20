c11cb957da05fbe08576aed441d84547f6f28021
Marten Sigwart <marten@okamaja.de>
2017-07-20 15:12:32 +0200

    added enum for different memory units

---
+++ src/main/java/ac/at/tuwien/infosys/fakeload/MemoryUnit.java
@@ -0,0 +1,23 @@
+package ac.at.tuwien.infosys.fakeload;
+
+/**
+ * Created by martensigwart on 19.07.17.
+ */
+public enum MemoryUnit {
+    BYTES, KB, MB, GB;
+
+    public long toBytes() {
+        switch (this) {
+            case BYTES:
+                return 1;
+            case KB:
+                return 1024;
+            case MB:
+                return 1024*1024;
+            case GB:
+                return 1024*1024*1024;
+            default:
+                return 0;
+        }
+    }
+}
