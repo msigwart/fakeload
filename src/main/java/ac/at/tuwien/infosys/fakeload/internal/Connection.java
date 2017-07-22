2415f59a0cbcd13efd721baa40ecc2e9654b6e9d
Marten Sigwart <marten@okamaja.de>
2017-07-22 13:19:32 +0200

    created class to act as shared object between FakeLoadDispatcher and simulation infrastructure

---
+++ src/main/java/ac/at/tuwien/infosys/fakeload/internal/Connection.java
@@ -0,0 +1,10 @@
+package ac.at.tuwien.infosys.fakeload.internal;
+
+/**
+ * Created by martensigwart on 04.07.17.
+ */
+public class Connection {
+    public void dispatchLoad() {
+        // TODO
+    }
+}
