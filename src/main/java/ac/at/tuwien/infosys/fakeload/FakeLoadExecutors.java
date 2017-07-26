5fb1ec1bc90d82b443191d6afaf1c4e60118b32e
Marten Sigwart <marten@okamaja.de>
2017-07-26 12:45:19 +0200

    created class with factory method for FakeLoadExecutor classes

---
+++ src/main/java/ac/at/tuwien/infosys/fakeload/FakeLoadExecutors.java
@@ -0,0 +1,25 @@
+package ac.at.tuwien.infosys.fakeload;
+
+import ac.at.tuwien.infosys.fakeload.internal.DefaultFakeLoadExecutor;
+import ac.at.tuwien.infosys.fakeload.internal.DefaultSimulationInfrastructure;
+import ac.at.tuwien.infosys.fakeload.internal.SimulationInfrastructure;
+
+/**
+ * Created by martensigwart on 26.07.17.
+ */
+public class FakeLoadExecutors {
+
+    private static SimulationInfrastructure defaultInfrastructure;
+
+    public static synchronized FakeLoadExecutor newDefaultExecutor() {
+        if (defaultInfrastructure == null) {
+            defaultInfrastructure = new DefaultSimulationInfrastructure();
+        }
+        return new DefaultFakeLoadExecutor(defaultInfrastructure);
+    }
+
+    // prevent instantiation by suppressing default constructor
+    private FakeLoadExecutors() {
+        throw new AssertionError();
+    }
+}
