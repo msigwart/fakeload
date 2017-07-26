8d713ac6425cd6f1000c948c0edc36abcc7c2f12
Marten Sigwart <marten@okamaja.de>
2017-07-26 12:42:45 +0200

    created interface and default implementation

--- src/main/java/ac/at/tuwien/infosys/fakeload/FakeLoadExecutor.java
+++ src/main/java/ac/at/tuwien/infosys/fakeload/FakeLoadExecutor.java
@@ -1,33 +1,55 @@
-package ac.at.tuwien.infosys.fakeload.internal;
-
-import ac.at.tuwien.infosys.fakeload.FakeLoad;
-
-import java.util.concurrent.Future;
+package ac.at.tuwien.infosys.fakeload;
 
 /**
- * Interface to represent a fakeload dispatcher. A fakeload dispatcher is responsible
- * for scheduling and dispatching load instructions contained in {@link FakeLoad} objects
- * to the simulation infrastructure.
+ * An object that executes submitted {@link FakeLoad} objects.
+ * This interface provides a way of decoupling FakeLoad submission from the mechanics of how a FakeLoad
+ * is actually simulated.
  *
  * <p>
- * Whenever a {@link FakeLoad#execute()} is called, the fake load gets propagated to
- * fake load dispatcher via {@link #submitLoad(FakeLoad)}. The dispatcher is responsible
- * for propagating the load instructions of the {@code FakeLoad} object to an underlying
- * simulation infrastructure.
+ * Implementations of this interface should take care of scheduling and executing load instructions
+ * contained in a FakeLoad as accurate as possible to provide a reliable simulation of system load.
  *
+ * <p>
+ * Note: In a 'realistic' scenario multiple {@code FakeLoad}s being executed simultaneously should
+ * produce a system load which is an aggregation of all load instructions contained in the
+ * {@code FakeLoad} objects. If thread A submits a FakeLoad of 20% CPU and thread B submits a
+ * FakeLoad of 30% CPU the resulting system load should be ~ 50%. However, different scenarios
+ * of how {@code FakeLoad}s are executed are valid as well. Therefore it is at the discretion of the
+ * {@code FakeLoadExecutor} implementation of how execution of {@code FakeLoad}s is handled in
+ * detail. To avoid confusion the exact behaviour should be clearly stated in the documentation
+ * of the implementing class.
+ *
+ * <p>
+ * The {@link FakeLoadExecutors} class provides convenient factory methods for different executors
+ * which should suffice for most simulation scenarios.
  *
  * @see FakeLoad
+ * @see FakeLoadExecutors
  * @since 1.8
  * @author Marten Sigwart
  */
-public interface FakeLoadDispatcher {
+public interface FakeLoadExecutor {
 
     /**
-     * Submits a {@code FakeLoad} object to an underlying simulation infrastructure.
+     * Executes the specified fake load.
      *
-     * @param load
-     * @return a Future representing the pending completion of the FakeLoad.
+     * <p>
+     * This means the 'fake' system load instructions (CPU, memory, other FakeLoad objects, etc.)
+     * contained withing the provided {@code FakeLoad} instance are simulated and executed by the executor.
+     * For example after calling {@code execute} with a FakeLoad object specifying aCPU load of 80% and
+     * a duration of 10 seconds, the executor will generate a CPU load of 80% for the next 10 seconds.
+     *
+     * <p>
+     * Note: This should be a blocking call, meaning within a single thread the method call blocks until
+     * the execution of a fake load has finished. This is due to the fact that a fake load represents
+     * a spaceholder for some real computation. A real computation would take all the time it needs
+     * (and would therefore block) until it has finished, therefore the fake computation should do so as well.
+     * However, different threads calling {@code execute()} should be able to do so at the same time.
+     *
+     * @param load the FakeLoad to be executed
+     * @since 1.8
      */
-    Future<String> submitLoad(FakeLoad load);
+    void execute(FakeLoad load);
+
 
 }
--- src/main/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutor.java
+++ src/main/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutor.java
@@ -1,50 +1,40 @@
 package ac.at.tuwien.infosys.fakeload.internal;
 
 import ac.at.tuwien.infosys.fakeload.FakeLoad;
+import ac.at.tuwien.infosys.fakeload.FakeLoadExecutor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.*;
 
 /**
- * Default implementation of the {@link FakeLoadDispatcher} interface. The dispatcher is responsible
- * for scheduling and dispatching fake loads to an underlying simulation infrastructure.
+ * A {@link FakeLoadExecutor} that executes each submitted {@link FakeLoad} by passing passing it on to an underlying
+ * {@link SimulationInfrastructure}.
  *
  * <p>
- * This concrete implementation consists of a {@link DefaultInfrastructure} instance and a scheduler.
- * The infrastructure is responsible for actually executing load instructions of {@link FakeLoad} objects
- * submitted via {@link #submitLoad(FakeLoad)}. The scheduler is responsible for scheduling the propagation of load
+ * This class consists of a {@code SimulationInfrastructure} instance and a scheduler.
+ * The infrastructure is responsible for actually executing load instructions of {@code FakeLoad} objects
+ * submitted via {@code execute(FakeLoad)}. The scheduler is responsible for scheduling the propagation of load
  * instructions to the infrastructure at the right time. Further the scheduler is also responsible for reducing the
  * load again once it has run.
  *
  * <p>
- * Multiple fake loads being executed simultaneously should produce a system load which is the aggregation of all
- * load instructions contained in these fake loads. If thread A executes a fake load of 20% CPU and thread B executes
- * a fake load of 30% CPU the resulting system load should be 20% + 30% = 50%.
- * The {@code DefaultFakeLoadDispatcher} is responsible for this aggregation as well as reporting any faults concerning
+ * Multiple {@code FakeLoad} objects being executed simultaneously produce a system load which is the aggregation of all
+ * load instructions contained in the objects. If thread A submits a {@code FakeLoad} of 20% CPU and thread B submits
+ * a {@code FakeLoad} of 30% CPU the resulting system load should be ~ 50%.
+ *
+ * <p>TODO NOT UP TO DATE ANYMORE
+ * The {@code DefaultFakeLoadExecutor} is responsible for this aggregation as well as reporting any faults concerning
  * any passing of load limitations of the system. For example executing a CPU load of more than 100% is not possible,
  * therefore if accumulated CPU load of the FakeLoad instances being executed exceeds that an error should be thrown.
  *
- * <p>
  *
- *
- * <p>
- * The simulation infrastructure consists of multiple threads each with a different simulation task.
- * For more information on the simulation infrastructure see {@link DefaultInfrastructure}.
- *
- * @see FakeLoad
- * @see FakeLoadDispatcher
  * @since 1.8
  * @author Marten Sigwart
  */
-public class DefaultFakeLoadDispatcher implements FakeLoadDispatcher {
+public final class DefaultFakeLoadExecutor implements FakeLoadExecutor {
 
-    private static final Logger log = LoggerFactory.getLogger(DefaultFakeLoadDispatcher.class);
-
-    private int cpuLoad;
-    private long memoryLoad;
-    private long diskIOLoad;
-    private long netIOLoad;
+    private static final Logger log = LoggerFactory.getLogger(DefaultFakeLoadExecutor.class);
 
 
     /**
@@ -55,29 +45,34 @@ public class DefaultFakeLoadDispatcher implements FakeLoadDispatcher {
     /**
      * Connection to the simulation infrastructure
      */
-    private final DefaultInfrastructure infrastructure;
+    private final SimulationInfrastructure infrastructure;
 
     /**
      * Constructor
      */
-    public DefaultFakeLoadDispatcher(DefaultInfrastructure infrastructure) {
+    public DefaultFakeLoadExecutor(SimulationInfrastructure infrastructure) {
         this.infrastructure = infrastructure;
         scheduler = new ScheduledThreadPoolExecutor(1);
     }
 
 
     @Override
-    public Future<String> submitLoad(FakeLoad load) {
-        return scheduleLoad(load);
+    public void execute(FakeLoad load) {
+        try {
+            Future<String> future = scheduleLoad(load);
+            future.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
-    private Future<String> scheduleLoad(FakeLoad fakeLoad) {
+    private Future<String> scheduleLoad(FakeLoad load) {
         log.debug("Scheduling load...");
         try {
-            infrastructure.increaseCpu(fakeLoad.getCpuLoad());
-            infrastructure.increaseMemory(fakeLoad.getMemoryLoad());
-            infrastructure.increaseDiskIO(fakeLoad.getDiskIOLoad());
-            infrastructure.increaseNetIO(fakeLoad.getNetIOLoad());
+            infrastructure.increaseCpu(load.getCpuLoad());
+            infrastructure.increaseMemory(load.getMemoryLoad());
+            infrastructure.increaseDiskIO(load.getDiskIOLoad());
+            infrastructure.increaseNetIO(load.getNetIOLoad());
 
         } catch (MaximumLoadExceededException e) {
             throw new RuntimeException(e.getMessage());
@@ -85,14 +80,14 @@ public class DefaultFakeLoadDispatcher implements FakeLoadDispatcher {
 
         Future<String> future = scheduler.schedule(() -> {
 
-            infrastructure.decreaseCpu(fakeLoad.getCpuLoad());
-            infrastructure.decreaseMemory(fakeLoad.getMemoryLoad());
-            infrastructure.decreaseDiskIO(fakeLoad.getDiskIOLoad());
-            infrastructure.decreaseNetIO(fakeLoad.getNetIOLoad());
+            infrastructure.decreaseCpu(load.getCpuLoad());
+            infrastructure.decreaseMemory(load.getMemoryLoad());
+            infrastructure.decreaseDiskIO(load.getDiskIOLoad());
+            infrastructure.decreaseNetIO(load.getNetIOLoad());
 
             return "";
 
-        }, fakeLoad.getDuration(), fakeLoad.getTimeUnit());
+        }, load.getDuration(), load.getTimeUnit());
 
 
         log.debug("Finished scheduling.");
