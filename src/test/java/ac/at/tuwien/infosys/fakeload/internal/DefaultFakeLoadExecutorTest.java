863c7d9743c0cd63f6330fa11113c29a66bd2c22
Marten Sigwart <marten@okamaja.de>
2017-07-29 18:51:46 +0200

    added test case

--- src/test/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutorTest.java
+++ src/test/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutorTest.java
@@ -1,8 +1,6 @@
 package ac.at.tuwien.infosys.fakeload.internal;
 
-import ac.at.tuwien.infosys.fakeload.FakeLoad;
-import ac.at.tuwien.infosys.fakeload.FakeLoadExecutor;
-import ac.at.tuwien.infosys.fakeload.FakeLoads;
+import ac.at.tuwien.infosys.fakeload.*;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -49,11 +47,54 @@ public class DefaultFakeLoadExecutorTest {
         long duration = 10;
         TimeUnit unit = TimeUnit.SECONDS;
         FakeLoad fakeLoad = FakeLoads.createLoad().lasting(duration, unit).withCpuLoad(20);
+        assertFakeLoadExecution(duration, unit, fakeLoad);
+    }
+
+
+
+    @Test
+    public void testComplexLoadExecution() {
+        long duration = 100;
+        TimeUnit unit = TimeUnit.MILLISECONDS;
+        FakeLoad parent = FakeLoads.createLoad().lasting(duration, unit)
+                .withCpuLoad(99)
+                .withMemoryLoad(9999, MemoryUnit.BYTES);
+
+        int noOfChildren = 10;
+        int noOfGrandChildrenPerChild = 9;
+
+        long startCPU = 1;
+        long startMemory = 1000;
+
+        // create children
+        for (int i=0; i<noOfChildren; i++) {
+            FakeLoad child = FakeLoads.createLoad().lasting(duration, unit)
+                    .withCpuLoad(startCPU*i)
+                    .withMemoryLoad(startMemory*i, MemoryUnit.KB);
+
+            // create grand children
+            for (int j=0; j<noOfGrandChildrenPerChild; j++) {
+                FakeLoad grandChild = FakeLoads.createLoad().lasting(duration, unit)
+                        .withCpuLoad(startCPU+j)
+                        .withMemoryLoad(startMemory*i+j, MemoryUnit.KB);
+
+                child = child.addLoad(grandChild);
+            }
+
+            parent = parent.addLoad(child);
+
+        }
+
+        assertFakeLoadExecution(duration+duration*noOfChildren*noOfGrandChildrenPerChild, unit, parent);
+    }
+
+
+    private void assertFakeLoadExecution(long expectedDuration, TimeUnit unit, FakeLoad fakeLoad) {
         long startTime = System.nanoTime();
         executor.execute(fakeLoad);
         long stopTime = System.nanoTime();
         long executedTime = stopTime -startTime;
-        Assert.assertTrue(executedTime >= unit.toNanos(duration));
+        Assert.assertTrue(executedTime >= unit.toNanos(expectedDuration));
     }
 
 }
