a739b2d0553761ae2daa2a5b6def178ce04cb30b
Marten Sigwart <marten@okamaja.de>
2017-07-29 17:38:16 +0200

    created unit test for DefaultFakeLoadExecutor

--- src/test/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutorTest.java
+++ src/test/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutorTest.java
@@ -1,7 +1,13 @@
 package ac.at.tuwien.infosys.fakeload.internal;
 
+import ac.at.tuwien.infosys.fakeload.FakeLoad;
 import ac.at.tuwien.infosys.fakeload.FakeLoadExecutor;
+import ac.at.tuwien.infosys.fakeload.FakeLoads;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Tests for class {@link DefaultFakeLoadExecutor}
@@ -9,10 +15,25 @@ import org.junit.Test;
  */
 public class DefaultFakeLoadExecutorTest {
 
+    private FakeLoadExecutor executor;
+
+    @Before
+    public void setUp() {
+        SimulationInfrastructure infrastructure = new DefaultSimulationInfrastructure();
+        FakeLoadScheduler scheduler = new DefaultFakeLoadScheduler(infrastructure);
+        executor = new DefaultFakeLoadExecutor(scheduler);
+    }
+
     @Test
     public void testSimpleLoadExecution() {
-        SimulationInfrastructure infrastructure = new DefaultSimulationInfrastructure();
-        FakeLoadExecutor executor = new DefaultFakeLoadExecutor(infrastructure);
+        long duration = 10;
+        TimeUnit unit = TimeUnit.SECONDS;
+        FakeLoad fakeLoad = FakeLoads.createLoad().lasting(duration, unit).withCpuLoad(20);
+        long startTime = System.nanoTime();
+        executor.execute(fakeLoad);
+        long stopTime = System.nanoTime();
+        long executedTime = stopTime -startTime;
+        Assert.assertTrue(executedTime >= unit.toNanos(duration));
     }
 
 }
