083b8e513cb944f30a1f5b39f700bc5ab3a7dcfb
Marten Sigwart <marten@okamaja.de>
2017-07-29 17:40:02 +0200

    added new dependency on FakeLoadScheduler

--- src/main/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutor.java
+++ src/main/java/ac/at/tuwien/infosys/fakeload/internal/DefaultFakeLoadExecutor.java
@@ -39,79 +39,27 @@ public final class DefaultFakeLoadExecutor implements FakeLoadExecutor {
 
     private static final Logger log = LoggerFactory.getLogger(DefaultFakeLoadExecutor.class);
 
+    private final FakeLoadScheduler scheduler;
 
-    /**
-     * Executor Service for scheduling simulation loads
-     */
-    private ScheduledThreadPoolExecutor scheduler;
-
-    /**
-     * Connection to the simulation infrastructure
-     */
-    private final SimulationInfrastructure infrastructure;
 
     /**
      * Constructor
      */
-    public DefaultFakeLoadExecutor(SimulationInfrastructure infrastructure) {
-        this.infrastructure = infrastructure;
-        scheduler = new ScheduledThreadPoolExecutor(1);
+    public DefaultFakeLoadExecutor(FakeLoadScheduler scheduler) {
+        this.scheduler = scheduler;
     }
 
 
     @Override
     public void execute(FakeLoad load) {
         try {
-            Future<Void> future = scheduleLoad(load);
+            Future<Void> future = scheduler.schedule(load);
             future.get();
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
 
-    private Future<Void> scheduleLoad(FakeLoad load) {
-        log.debug("Scheduling load...");
-
-
-        // TODO not correct yet
-        Future<Void> future = null;
-        long offset = 0L;
-        for (FakeLoad f: load) {
-            scheduleIncrease(f, offset);
-            offset += f.getTimeUnit().toMillis(f.getDuration());
-            future = scheduleDecrease(f, offset);
-        }
-
-        log.debug("Finished scheduling.");
-        return future;
-    }
-
-
-    private Future<Void> scheduleDecrease(FakeLoad load, long offset) {
-        return scheduler.schedule(() -> {
-
-            infrastructure.decreaseCpu(load.getCpuLoad());
-            infrastructure.decreaseMemory(load.getMemoryLoad());
-            infrastructure.decreaseDiskIO(load.getDiskIOLoad());
-            infrastructure.decreaseNetIO(load.getNetIOLoad());
-
-            return null;
-
-        }, offset, TimeUnit.MILLISECONDS);
-    }
-
-    private Future<Void> scheduleIncrease(FakeLoad load, long offset) {
-        return scheduler.schedule(() -> {
-
-            infrastructure.increaseCpu(load.getCpuLoad());
-            infrastructure.increaseMemory(load.getMemoryLoad());
-            infrastructure.increaseDiskIO(load.getDiskIOLoad());
-            infrastructure.increaseNetIO(load.getNetIOLoad());
-
-            return null;
-        }, offset, TimeUnit.MILLISECONDS);
-
-    }
 
 
 }
