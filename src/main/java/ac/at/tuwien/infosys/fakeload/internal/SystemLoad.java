0570554e49ee93bad4cbd873b9445cefd289d423
Marten Sigwart <marten@okamaja.de>
2017-07-31 19:50:16 +0200

    changed variable names

--- src/main/java/ac/at/tuwien/infosys/fakeload/internal/SystemLoad.java
+++ src/main/java/ac/at/tuwien/infosys/fakeload/internal/SystemLoad.java
@@ -3,52 +3,52 @@ package ac.at.tuwien.infosys.fakeload.internal;
 import ac.at.tuwien.infosys.fakeload.FakeLoad;
 
 import javax.annotation.concurrent.GuardedBy;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Created by martensigwart on 04.07.17.
  */
-final class Connection {
+@ThreadSafe
+final class SystemLoad {
 
 
-    @GuardedBy("this") private long cpuLoad;
-    @GuardedBy("this") private long memoryLoad;
-    @GuardedBy("this") private long diskIOLoad;
-    @GuardedBy("this") private long netIOLoad;
+    @GuardedBy("this") private long cpu;
+    @GuardedBy("this") private long memory;
+    @GuardedBy("this") private long diskIO;
+    @GuardedBy("this") private long netIO;
 
 
-    Connection() {
-        cpuLoad     = 0L;
-        memoryLoad  = 0L;
-        diskIOLoad  = 0L;
-        netIOLoad   = 0L;
+    SystemLoad() {
+        cpu     = 0L;
+        memory  = 0L;
+        diskIO  = 0L;
+        netIO   = 0L;
     }
 
-    synchronized long getCpuLoad() {
-        return cpuLoad;
+    synchronized long getCpu() {
+        return cpu;
     }
 
-    synchronized long getMemoryLoad() {
-        return memoryLoad;
+    synchronized long getMemory() {
+        return memory;
     }
 
-    synchronized long getDiskIOLoad() {
-        return diskIOLoad;
+    synchronized long getDiskIO() {
+        return diskIO;
     }
 
-    synchronized long getNetIOLoad() {
-        return netIOLoad;
+    synchronized long getNetIO() {
+        return netIO;
     }
 
     // TODO is synchronized enough synchronization
     synchronized void increaseBy(FakeLoad load) throws MaximumLoadExceededException {
         checkMaximumLoadNotExceeded(load);
 
-        this.cpuLoad    += load.getCpuLoad();
-        this.memoryLoad += load.getMemoryLoad();
-        this.diskIOLoad += load.getDiskIOLoad();
-        this.netIOLoad  += load.getNetIOLoad();
+        this.cpu    += load.getCpuLoad();
+        this.memory += load.getMemoryLoad();
+        this.diskIO += load.getDiskIOLoad();
+        this.netIO  += load.getNetIOLoad();
 
     }
 
@@ -56,35 +56,35 @@ final class Connection {
     synchronized void decreaseBy(FakeLoad load) {
         checkNotBelowMinimumLoad(load);
 
-        this.cpuLoad    -= load.getCpuLoad();
-        this.memoryLoad -= load.getMemoryLoad();
-        this.diskIOLoad -= load.getDiskIOLoad();
-        this.netIOLoad  -= load.getNetIOLoad();
+        this.cpu -= load.getCpuLoad();
+        this.memory -= load.getMemoryLoad();
+        this.diskIO -= load.getDiskIOLoad();
+        this.netIO -= load.getNetIOLoad();
 
     }
 
 
     private synchronized void checkMaximumLoadNotExceeded(FakeLoad load) throws MaximumLoadExceededException {
-        if (this.cpuLoad + load.getCpuLoad() > 100)
-            throw new MaximumLoadExceededException(String.format("Increase by %d would cause a CPU load of over 100%", load.getCpuLoad()));
+        if (this.cpu + load.getCpuLoad() > 100)
+            throw new MaximumLoadExceededException(String.format("Increase by %d would cause a CPU load of over 100%%", load.getCpuLoad()));
 
         // check other load limits
     }
 
     private synchronized void checkNotBelowMinimumLoad(FakeLoad load) {
-        if (this.cpuLoad - load.getCpuLoad() < 0) {
+        if (this.cpu - load.getCpuLoad() < 0) {
             throw new RuntimeException(String.format("Decrease by %d would cause a negative CPU load", load.getCpuLoad()));
         }
 
-        if (this.memoryLoad - load.getMemoryLoad() < 0) {
+        if (this.memory - load.getMemoryLoad() < 0) {
             throw new RuntimeException(String.format("Decrease by %d would cause a negative memory load", load.getMemoryLoad()));
         }
 
-        if (this.diskIOLoad - load.getDiskIOLoad() < 0) {
+        if (this.diskIO - load.getDiskIOLoad() < 0) {
             throw new RuntimeException(String.format("Decrease by %d would cause a negative disk IO load", load.getDiskIOLoad()));
         }
 
-        if (this.netIOLoad - load.getNetIOLoad() < 0) {
+        if (this.netIO - load.getNetIOLoad() < 0) {
             throw new RuntimeException(String.format("Decrease by %d would cause a negative net IO load", load.getNetIOLoad()));
         }
     }
