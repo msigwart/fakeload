52e77fc1247811ee02d9b29abf1c657ad0e95c00
Marten Sigwart <marten@okamaja.de>
2017-08-09 22:56:00 +0000

    implemented builder

--- src/main/java/com/martensigwart/fakeload/FakeLoadBuilder.java
+++ src/main/java/com/martensigwart/fakeload/FakeLoadBuilder.java
@@ -1,83 +1,95 @@
 package com.martensigwart.fakeload;
 
-import com.google.gson.Gson;
-
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Created by martensigwart on 19.07.17.
+ * A {@code FakeLoad} builder.
+ * Can be used as convenient tool to create FakeLoad instances more efficiently.
+ * <p>
+ * Both implementations of the {@link FakeLoad} interface, {@link SimpleFakeLoad} and {@link CompositeFakeLoad}
+ * are immutable classes. Therfore using the provided fluent interface to set load parameters
+ * a new instance is created on every call. Using the builder especially bigger {@code FakeLoad}
+ * instances can be created more efficiently.
+ *
+ * @since 1.8
+ * @author Marten Sigwart
  */
 public class FakeLoadBuilder {
 
-        // Required parameters
-        private final long duration;
-        private final TimeUnit unit;
-        private final String json;
-
-        // Optional parameters
+        // simple load parameters
+        private long duration       = 0L;
+        private TimeUnit unit       = TimeUnit.MILLISECONDS;
         private int repetitions     = 0;
-        private int cpuLoad         = 0;
+        private long cpuLoad        = 0;
         private long memoryLoad     = 0L;
         private long diskIOLoad     = 0L;
         private long netIOLoad      = 0L;
 
+        // inner loads
+        private List<FakeLoad> innerLoads = new ArrayList<>();
 
 
         public FakeLoadBuilder(long duration, TimeUnit unit) {
             this.duration = duration;
             this.unit = unit;
-            this.json = null;
         }
 
         public FakeLoadBuilder(String json) {
-            this.json = json;
-            this.duration = -1L;
-            this.unit = null;
+            //TODO parse json
         }
 
         public FakeLoadBuilder() {
-            this.duration = -1L;
-            this.unit = null;
-            this.json = null;
+            // empty
         }
 
 
-        public FakeLoadBuilder repetitions(int repetitions) {
+        public FakeLoadBuilder repeat(int repetitions) {
             this.repetitions = repetitions;
             return this;
         }
 
-        public FakeLoadBuilder cpuLoad(int cpuLoad) {
+        public FakeLoadBuilder withCpuLoad(int cpuLoad) {
             this.cpuLoad = cpuLoad;
             return this;
         }
 
-        public FakeLoadBuilder memoryLoad(long memoryLoad) {
-            this.memoryLoad = memoryLoad;
+        public FakeLoadBuilder withMemoryLoad(long memoryLoad, MemoryUnit unit) {
+            this.memoryLoad = unit.toBytes(memoryLoad);
             return this;
         }
 
-        public FakeLoadBuilder diskIOLoad(long diskIOLoad) {
+        public FakeLoadBuilder withDiskIO(long diskIOLoad) {
             this.diskIOLoad = diskIOLoad;
             return this;
         }
 
-        public FakeLoadBuilder netIOLoad(long netIOLoad) {
+        public FakeLoadBuilder withNetIO(long netIOLoad) {
             this.netIOLoad = netIOLoad;
             return this;
         }
 
+        public FakeLoadBuilder addLoad(FakeLoad load) {
+            innerLoads.add(load);
+            return this;
+        }
+
+        public FakeLoadBuilder addLoads(Collection<FakeLoad> loads) {
+            innerLoads.addAll(loads);
+            return this;
+        }
+
+
         public FakeLoad build() {
-
-            // TODO: Create FakeLoad from JSON
-            if (json != null) {
-                Gson g = new Gson();
-                FakeLoad pattern = g.fromJson(json, SimpleFakeLoad.class);
-                return new SimpleFakeLoad();
+            if (innerLoads.isEmpty()) {
+                return new SimpleFakeLoad(duration, unit, repetitions, cpuLoad, memoryLoad, diskIOLoad, netIOLoad);
+            } else {
+                return new CompositeFakeLoad(
+                        new SimpleFakeLoad(duration, unit, 0, cpuLoad, memoryLoad, diskIOLoad, netIOLoad),
+                        innerLoads, repetitions);
             }
-
-            // TODO: Create LoadPattern from load parameters
-            return new SimpleFakeLoad();
         }
 
 }
