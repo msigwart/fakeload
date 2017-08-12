58c1d99b85d3d5a372907e4ad0a153301fd35b07
Marten Sigwart <marten@okamaja.de>
2017-08-12 17:47:58 +0000

    added some more fakelaods

--- src/test/java/com/martensigwart/fakeload/SomeClass.java
+++ src/test/java/com/martensigwart/fakeload/SomeClass.java
@@ -12,18 +12,18 @@ public class SomeClass {
 
     private static final Logger log = LoggerFactory.getLogger(SomeClass.class);
 
+    private FakeLoadExecutor executor = FakeLoadExecutors.newDefaultExecutor();
+
     public void someMethod() {
         log.info("Entered someMethod()");
 
         // Execute FakeLoad
 //        FakeLoad fakeLoad = FakeLoads.createLoad(100, "50%", "1024m");
-        FakeLoadExecutor executor = FakeLoadExecutors.newDefaultExecutor();
-        FakeLoad fakeLoad = FakeLoads.createLoad().lasting(10, TimeUnit.SECONDS)
+        FakeLoad fakeLoad = FakeLoads.createLoad().lasting(60, TimeUnit.SECONDS)
                 .withCpuLoad(50)
                 .withMemoryLoad(2, MemoryUnit.GB);
 
         executor.execute(fakeLoad);
-
         // some code
         log.info("Leaving someMethod()...");
     }
@@ -31,22 +31,26 @@ public class SomeClass {
     public void someOtherMethod() {
         log.info("Entered someOtherMethod()");
 
-        // Create LoadPattern with Builder pattern
-//        FakeLoad fakeload = new FakeLoadBuilder().build();
-//        fakeload.addLoad(60, TimeUnit.SECONDS, "30%", "2048k");
-//        fakeload.addIntervalLoad(200, TimeUnit.MILLISECONDS, 10, TimeUnit.SECONDS, "50%", "1024m");
+        // Create FakeLoad with Builder pattern
+        FakeLoad fakeload = new FakeLoadBuilder()
+                .lasting(60, TimeUnit.SECONDS)
+                .withCpuLoad(30)
+                .withMemoryLoad(1024, MemoryUnit.MB).build();
 
-        // Execute AbstractFakeLoad
-//        AbstractFakeLoad fakeLoad = new AbstractFakeLoad(pattern);
-//        fakeLoad.execute();
-//
-//        pattern = new FakeLoadBuilder(10, TimeUnit.SECONDS)
-//                .withCpuLoad(50).withMemoryLoad(600).build();
-//
-//        fakeLoad.setLoad(pattern);
-//        fakeLoad.execute();
-//
-//
+        // Execute FakeLoad
+        executor.execute(fakeload);
+
+        fakeload = new FakeLoadBuilder(10, TimeUnit.SECONDS)
+                .withCpuLoad(10).withMemoryLoad(100, MemoryUnit.KB)
+                .addLoad(new FakeLoadBuilder(10, TimeUnit.SECONDS)
+                    .withCpuLoad(20).withMemoryLoad(200, MemoryUnit.KB).build())
+                .addLoad(new FakeLoadBuilder(10, TimeUnit.SECONDS)
+                    .withCpuLoad(30).withMemoryLoad(300, MemoryUnit.KB).build())
+                .build();
+
+        executor.execute(fakeload);
+
+
 //        // Create LoadPattern with Factory pattern
 //        pattern = LoadPatterns.createLoadPattern();
 //        pattern.addLoad(60, TimeUnit.SECONDS, "30%", "2048k");
@@ -63,8 +67,8 @@ public class SomeClass {
     public void yetAnotherMethod() {
         log.info("Entered yetAnotherMethod()");
 
-        FakeLoadExecutor executor = FakeLoadExecutors.newDefaultExecutor();
         FakeLoad fakeLoad = FakeLoads.createLoad()
+                .lasting(30, TimeUnit.SECONDS)
                 .withCpuLoad(80)
                 .withMemoryLoad(300, MemoryUnit.MB);
 
