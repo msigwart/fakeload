6d07e0c70e30e0929479e33417c7d745f0312c1a
Marten Sigwart <marten@okamaja.de>
2017-08-20 18:28:03 +0200

    created test for DefaultSimulationInfrastructure and LoadController

---
+++ src/test/java/com/martensigwart/fakeload/DefaultSimulationInfrastructureTest.java
@@ -0,0 +1,48 @@
+package com.martensigwart.fakeload;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Test class for class {@link DefaultSimulationInfrastructure}
+ */
+public class DefaultSimulationInfrastructureTest {
+
+    LoadController mockController;
+    DefaultSimulationInfrastructure infrastructure;
+
+    @Before
+    public void setUp() {
+
+        mockController = mock(LoadController.class);
+
+        infrastructure = new DefaultSimulationInfrastructure(Executors.newFixedThreadPool(1), mockController);
+    }
+
+    @Test
+    public void increaseSystemLoadBy() throws Exception {
+        FakeLoad fakeLoad = FakeLoads.createLoad().lasting(10, TimeUnit.SECONDS).withCpu(50);
+
+        infrastructure.increaseSystemLoadBy(fakeLoad);
+
+        verify(mockController).increaseSystemLoadBy(fakeLoad);
+    }
+
+    @Test
+    public void decreaseSystemLoadBy() throws Exception {
+        FakeLoad fakeLoad = FakeLoads.createLoad().lasting(10, TimeUnit.SECONDS).withCpu(50);
+
+        infrastructure.decreaseSystemLoadBy(fakeLoad);
+
+        verify(mockController).decreaseSystemLoadBy(fakeLoad);
+    }
+
+}
---
+++ src/test/java/com/martensigwart/fakeload/LoadControllerTest.java
@@ -0,0 +1,22 @@
+package com.martensigwart.fakeload;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests for class {@link LoadController}
+ */
+public class LoadControllerTest {
+
+    @Test
+    public void increaseSystemLoadBy() throws Exception {
+
+    }
+
+    @Test
+    public void decreaseSystemLoadBy() throws Exception {
+
+    }
+
+}
