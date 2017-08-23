77ee2d6bb6155b49754cf226092aef336dfdbced
Marten Sigwart <marten@okamaja.de>
2017-08-23 19:27:34 +0200

    defined two methods simulateLoad and waitConditionFulfilled

--- src/main/java/com/martensigwart/fakeload/LoadSimulator.java
+++ src/main/java/com/martensigwart/fakeload/LoadSimulator.java
@@ -1,9 +1,25 @@
 package com.martensigwart.fakeload;
 
-/**
- * Created by martensigwart on 03.08.17.
- */
+
 public interface LoadSimulator extends Runnable {
+
+    /**
+     * Simulates load.
+     *
+     * @throws InterruptedException when the thread is interrupted during load simulation
+     */
+    void simulateLoad(long load) throws InterruptedException;
+
+    /**
+     * Represents the 'wait' condition of a {@code LoadSimulator}.
+     * <p>
+     * In order to save resources, a LoadSimulator should wait when
+     * there is nothing to simulate. For example, when a Memory Simulator
+     * already allocated the desired amount of memory, it can wait for new
+     * memory instructions -> It has nothing to do. This method represents the state "nothing to do".
+     * @return returns true if there is nothing to do and false otherwise.
+     */
+    boolean waitConditionFulfilled();
 
     long getLoad();
     void setLoad(long desiredLoad);
