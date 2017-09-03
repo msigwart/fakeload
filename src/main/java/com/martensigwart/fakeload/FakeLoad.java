4ff49175c17a426baf074deb5aec27f27f643edd
Marten Sigwart <marten@okamaja.de>
2017-09-03 14:08:18 +0200

    updated javadoc

--- src/main/java/com/martensigwart/fakeload/FakeLoad.java
+++ src/main/java/com/martensigwart/fakeload/FakeLoad.java
@@ -6,19 +6,23 @@ import java.util.Iterator;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Represents the main API for the FakeLoad Library.
- * With instances of this type clients decide what kinds of system loads they want to generate and for how long.
+ * An object containing instructions for simulating system load.
+ *
+ * A {@code FakeLoad}, next to a {@link FakeLoadExecutor}, represents one of the two
+ * main concepts of the FakeLoad Library. With instances of this type clients decide
+ * what kinds of system loads they want to generate and for how long.
  *
  * <p>
  * Clients can specify what kind of load (e.g. CPU, RAM, …), how much of it (e.g. 80%, 1024MB, …)
  * and for how long a specific load (e.g. 10 s, 100 ms, …) is being simulated.
  * Also, a fake load can itself consist of other {@code FakeLoad} instances thus
- * giving the client the possibility to create complex patterns of system load instructions.
+ * giving the client the possibility to create more elaborate patterns of system load instructions.
  * </p>
  *
  * <p>
- * Note: All setter methods return a {@code FakeLoad} instance themselves, thus allowing a kind
+ * All setter methods of {@code FakeLoad} return a {@code FakeLoad} instance themselves, thus allowing a kind
  * of fluent interface like this:
+ *
  * <pre>
  * {@code FakeLoad fakeload = FakeLoads.create()
  *      .lasting(10, TimeUnit.SECONDS)
@@ -27,14 +31,29 @@ import java.util.concurrent.TimeUnit;
  *      .withDiskInput(10, MemoryUnit.KB)
  *      .withDiskOutput(20, MemoryUnit.MB);
  * }</pre>
- * Depending on whether the underlying {@code FakeLoad} implementation is mutable or immutable
- * these methods can either merely set specific load parameters or return a completely new
- * {@code FakeLoad} instance containing the new parameters, respectively.
+ *
+ * Class {@link FakeLoads} offers factory methods for creating {@code FakeLoad} instances.
+ * Class {@link FakeLoadBuilder} is an implementation of the builder pattern to create {@code FakeLoad instances}.
+ *
+ * Note: {@code FakeLoad} objects created with classes {@code FakeLoads} and {@code FakeLoadBuilder}
+ * are <b>immutable</b>. Thus, calling any of the setter methods will return newly constructed {@code FakeLoad}
+ * instances, containing the load parameters of the "old" {@code FakeLoad} object, as well as the newly passed
+ * load parameters. Therefore changing the load parameter of an existing {@code FakeLoad} object should
+ * always be done like this:
+ *
+ * <pre>
+ * // The original
+ * {@code} FakeLoad fakeload = FakeLoads.create().lasting(10, TimeUnit.SECONDS.withCpu(...
+ *
+ * // "Modifying" the original
+ * fakeload = fakeload.withCpu(50);
+ * </pre>
  *
  *
  * @since 1.8
- * @see FakeLoads
  * @see FakeLoadExecutor
+ * @see FakeLoads
+ * @see FakeLoadBuilder
  * @author Marten Sigwart
  */
 public interface FakeLoad extends Iterable<FakeLoad>, Serializable {
@@ -95,10 +114,26 @@ public interface FakeLoad extends Iterable<FakeLoad>, Serializable {
      */
     FakeLoad withDiskOutput(long load, MemoryUnit unit);
 
+    /**
+     * Adds the specified {@code FakeLoad} object to this {@code FakeLoad}'s inner loads.
+     * @param load the {@code FakeLoad} to be added to the inner loads
+     * @return returns the {@code FakeLoad} including the newly added {@code FakeLoad}
+     */
     FakeLoad addLoad(FakeLoad load);
 
+    /**
+     * Adds the specified {@code FakeLoad} objects to this {@code FakeLoad}'s inner loads.
+     * @param loads the {@code FakeLoad} objects to be added to the inner loads
+     * @return returns the {@code FakeLoad} including the newly added {@code FakeLoad} objects
+     */
     FakeLoad addLoads(Collection<FakeLoad> loads);
 
+    /**
+     * Determines whether the specified {@code FakeLoad} object in contained within the {@code FakeLoad}
+     * on which this method was called.
+     * @param load the {@code FakeLoad} object to look for
+     * @return true if {@code load} is contained within this {@code FakeLoad}
+     */
     boolean contains(FakeLoad load);
 
     /**
@@ -107,18 +142,49 @@ public interface FakeLoad extends Iterable<FakeLoad>, Serializable {
      */
     Collection<FakeLoad> getInnerLoads();
 
+    /**
+     * Returns the CPU load in percent
+     * @return an {@code int} containing the CPU load in percent
+     */
     int getCpu();
 
+    /**
+     * Returns the memory load in bytes
+     * @return a {@code long} containing the memory load in bytes
+     */
     long getMemory();
 
+    /**
+     * Returns the disk input load in bytes per second
+     * @return a {@code long} containing the disk input load in bytes per second
+     */
     long getDiskInput();
 
+    /**
+     * Returns the disk output load in bytes per second
+     * @return a {@code long} containing the disk output load in bytes per second
+     */
     long getDiskOutput();
 
+    /**
+     * Returns the duration of this {@code FakeLoad} system load configuration.
+     * The unit of the duration is determined by {@link #getTimeUnit()}.
+     * @return the duration of this {@code FakeLoad}'s load instructions
+     */
     long getDuration();
 
+    /**
+     * Returns the time unit of the duration specified by {@link #getDuration()}.
+     * @return the time unit
+     */
     TimeUnit getTimeUnit();
 
+    /**
+     * Returns the number of desired repetitions of this {@code FakeLoad} object.
+     * The number of repetitions decides how many times the {@code FakeLoad} object
+     * will be repeated during execution
+     * @return the number of repetitions
+     */
     int getRepetitions();
 
     /**
